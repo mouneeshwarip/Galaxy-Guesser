cf8e94b5a771100743d9d65e972e2997007a9ee3
Mouneeshwari Panneerselvam <mouneeshwari.panneerselvam@gmail.com>
2024-06-05 12:05:47 +0000

    add game rules based on user input

--- .vscode/intro.py
+++ .vscode/intro.py
@@ -23,7 +23,7 @@ def display_logo():
     print(logo)
 
 # function to display rules
-
+'''
 def display_rules():
     rules = f"""
 {Fore.GREEN}Rules:{Style.RESET_ALL}
@@ -34,4 +34,33 @@ def display_rules():
 5. Try to guess the word before the battery runs out!
 """
     print(rules)
-       
+
+    '''  
+
+def display_rules():
+    '''
+    Requesting user to provide input whether
+    user wants to read the game rules.
+    '''
+    while True:
+        try:
+            user_input = input("Do you want to read the rules?(Y/N)\n").lower()
+            if user_input == 'y':
+                print(f"""
+                {Fore.GREEN}Rules:{Style.RESET_ALL}
+
+                 1. Choose a level to play: 1, 2, or 3.
+                 2. Guess the letters to complete the word.
+                 3. You have a limited number of wrong guesses based on the level.
+                 4. Each wrong guess reduces the spaceship's battery level.
+                 5. Try to guess the word before the battery runs out!\n
+                 """)
+                return True
+                print(rules)
+            elif user_input == 'n':
+                print("Okay!, Lets Start!")
+                return False
+            else:
+                raise ValueError
+        except ValueError:
+            typewriter_effects("Invalid choice: Please enter 'Y' or 'N'.\n")  
