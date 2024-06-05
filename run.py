47f57ec77078715e54241f50790d2e1fe0136081
Mouneeshwari Panneerselvam <mouneeshwari.panneerselvam@gmail.com>
2024-06-05 14:29:42 +0000

    adjust print messages alignment and removed commented-out code

--- run.py
+++ run.py
@@ -5,14 +5,12 @@ from spaceship import print_spaceship, clear_terminal
 from intro import display_logo, display_rules
 
 def main_game():
-
-    # word lists for different levels
+   # word lists for different levels
     wordlist_level1 = ['universe', 'galaxy', 'milkyway', 'planet', 'star', 'comet']
     wordlist_level2 = ['astronaut', 'telescope', 'satellite', 'nebula', 'orbit', 'gravity']
     wordlist_level3 = ['constellation', 'interstellar', 'microgravity',  'blackhole', 'exoplanet', 'quasar']
-
-
-    # maximum wrong guesses for different levels
+   
+   # maximum wrong guesses for different levels
     max_wrongguesses_level1 = 6
     max_wrongguesses_level2 = 5
     max_wrongguesses_level3 = 4
@@ -25,13 +23,13 @@ def main_game():
     def choose_level():
         while True:
             try:
-                level = int(input("Which level would you like to play? Choose 1, 2, or 3:  \n"))
+                level = int(input(" Which level would you like to play? Choose 1, 2, or 3:  \n"))
                 if level in [1, 2, 3]:
                     return level
                 else:
-                    print("Invalid input! Please enter 1, 2, or 3.")
+                    print(" Invalid input! Please enter 1, 2, or 3.\n")
             except ValueError:
-                print("Invalid input! Please enter a number (1, 2, or 3).")
+                print(" Invalid input! Please enter a number (1, 2, or 3).\n")
     level = choose_level() 
 
     # Setting word list and max wrong guesses based on chosen level
@@ -47,16 +45,16 @@ def main_game():
 
     # choose a random word from the list
     randomword=random.choice(wordlist_level1)
-    print("\n" + "_ " * len(randomword))
-    print("\nGuess this word correctly and travel to space with our spaceship")
+    print(" \n" + "_ " * len(randomword))
+    print(" \nGuess this word correctly and travel to space with our spaceship\n")
     print(f""" {Fore.GREEN}
-        .'.  
+         .'.  
         |o o|
-    _| = |_
-    |       |
-    |_______|
+       _| = |_
+      |       |
+      |_______|
 
-    Battery-level : |||||| <---- This indicates the battery level of the spaceship. Finish the game before exhausting the battery. {Style.RESET_ALL}""")   
+      Battery-level : |||||| <---- This indicates the battery level of the spaceship{Style.RESET_ALL}""")   
 
     def printword(guessedletters):
         counter=0 ##index position
@@ -78,63 +76,59 @@ def main_game():
 
 
     while(amount_of_timeswrong != max_wrongguesses and current_letters_right != len(randomword)):
-        '''
-        print("\n Letters Guessed so far: ")
-        for letter in current_letters_guessed:
-            print(letter, end=" ")
-        letterguessed=input("\n Guess a letter: \n").lower() ##convert input to lower
-        '''
         if not first_iteration:
-            print("\n Letters Guessed so far: ")
+            print(" \n Letters Guessed so far: \n")
             for letter in current_letters_guessed:
-                print(letter, end=" ")
+                    print(  letter, end=" ")
         else:
             first_iteration = False
 
-        letterguessed = input("\n Guess a letter: \n").lower()  # Convert input to lower
+        letterguessed = input(" \n Guess a letter: \n").lower()  # Convert input to lower
 
-        ##check if the input is a valid alphabetical letter and the length of the letter
+        # check if the input is a valid alphabetical letter and the length of the letter
         if not letterguessed.isalpha() or len(letterguessed) !=1:
-            print("Invalid input! Please enter an alphabetical letter.")
+            print(" Invalid input! Please enter an alphabetical letter.\n")
             continue
         if letterguessed in current_letters_guessed:
-            print("You already guessed that letter!")
+            print(" You already guessed that letter!\n")
             continue
         current_letters_guessed.append(letterguessed) 
-        ##when the user is right
+        # when the user is right
         remaining_guesses=max_wrongguesses-amount_of_timeswrong
         if letterguessed in randomword:
             print_spaceship(level,remaining_guesses)
             current_letters_right =printword(current_letters_guessed)
-        ##when the user is wrong
+        # when the user is wrong
         else:
             amount_of_timeswrong+=1
             remaining_guesses=max_wrongguesses-amount_of_timeswrong
-            print(f"Wrong guess!! You are allowed to make {remaining_guesses} more wrong guesses")   
+            print(f" Wrong guess!! You are allowed to make {remaining_guesses} more wrong guesses\n")   
             print_spaceship(level,remaining_guesses)
             printword(current_letters_guessed)    
     
     if current_letters_right == len(randomword):
-        print("\nCongratulations! You guessed the word:", randomword)
+        print(" \nCongratulations! You guessed the word:\n", randomword)
     else:
-        print("\nGame is over! The word was:", randomword)
+        print(" \nGame is over! The word was:\n", randomword)
+
+#if the user continue to play        
 def play_again():
     while True:
-        play_again = input("Do you want to play again? (Y/N): ").strip().lower()
+        play_again = input(" Do you want to play again? (Y/N):\n ").strip().lower()
         if play_again == 'y':
             return True
         elif play_again == 'n':
             return False
         else:
-            print("Invalid input! Please enter 'Y' or 'N'.")
+            print(" Invalid input! Please enter 'Y' or 'N'.\n")
             
-
 # Display the logo and rules
 display_logo()
 display_rules()
 
+#perpetual loop for the main game
 while True:
     main_game()
     if not play_again():
-        print("Thank you for playing!")
+        print(" Thank you for playing!\n")
         break   
