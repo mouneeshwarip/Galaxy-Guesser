88f012090ff30c0db5be495e2612c269eb4b77c4
Mouneeshwari Panneerselvam <mouneeshwari.panneerselvam@gmail.com>
2024-06-05 11:15:08 +0000

    add clear_terminal function

--- run.py
+++ run.py
@@ -2,3 +2,9 @@ import os
 import random
 
 wordlist_level1 = ['universe', 'galaxy', 'milkyway', 'planet', 'star', 'comet']
+
+max_wrongguesses_level1 = 6
+
+# Clears the terminal
+def clear_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
