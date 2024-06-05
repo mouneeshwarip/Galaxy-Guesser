183ceef36c69428efba7f6e872e80b55eaffb045
Mouneeshwari Panneerselvam <mouneeshwari.panneerselvam@gmail.com>
2024-06-05 11:35:56 +0000

    define variables to the print_spaceship function

--- .vscode/spaceship.py
+++ .vscode/spaceship.py
@@ -2,3 +2,30 @@ import os
 
 def clear_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
+
+def print_spaceship(level,wrong):
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    RESET = '\033[0m'
+    if wrong != 0:
+         spaceship = f"""
+     .'.  
+    |o o|
+   _| = |_
+  |       |
+  |_______|
+
+  Battery-level : {"|"*wrong}    
+    """
+    else:
+         spaceship = f"""
+     .'.  
+    |o o|
+   _| = |_
+  |       |
+  |_______|
+
+  Battery-level : 0    
+    """
+    
